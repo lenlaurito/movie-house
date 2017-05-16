cfffe42d2711b0bffe5e7c94616fea624f01aaf0
sbanjoe25 <sbanjoe25@gmail.com>
2017-05-16 12:49:49 +0800

    add Getter and Setter for entity models.
    Signed-off-by: sbanjoe25 <sbanjoe25@gmail.com>

--- src/main/java/com/synacy/moviehouse/schedule/Schedule.java
+++ src/main/java/com/synacy/moviehouse/schedule/Schedule.java
@@ -1,28 +1,38 @@
 package com.synacy.moviehouse.schedule;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.synacy.moviehouse.cinema.Cinema;
 import com.synacy.moviehouse.movie.Movie;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
+@Getter
+@EqualsAndHashCode(of = {"id"})
+@JsonSerialize(using = ScheduleSerializer.class)
 @Entity
 public class Schedule {
 
 	@Id @GeneratedValue
 	private Long id;
 
+	@Setter
 	@NotNull @OneToOne(fetch = FetchType.LAZY)
 	private Movie movie;
 
-	@NotNull @OneToOne(fetch = FetchType.LAZY)
+	@Setter @NotNull @OneToOne(fetch = FetchType.LAZY)
 	private Cinema cinema;
 
-	@NotNull
+	@Setter @NotNull
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss Z", timezone = "GMT")
 	private Date startDateTime;
 
-	@NotNull
+	@Setter @NotNull
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss Z", timezone = "GMT")
 	private Date endDateTime;
-
 }
