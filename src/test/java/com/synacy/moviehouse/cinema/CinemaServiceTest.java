2835f401e2013af4a2653ea2c1c70f5334937247
sbanjoe25 <sbanjoe25@gmail.com>
2017-05-18 19:29:18 +0800

    add assertions to movie, cinema and schedule service tests
    Signed-off-by: sbanjoe25 <sbanjoe25@gmail.com>

--- src/test/java/com/synacy/moviehouse/cinema/CinemaServiceTest.java
+++ src/test/java/com/synacy/moviehouse/cinema/CinemaServiceTest.java
@@ -1,10 +1,14 @@
 package com.synacy.moviehouse.cinema;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -29,28 +33,64 @@ public class CinemaServiceTest {
 
     @Test
     public void fetchAllCinema_shouldReturnAllCinemaWithNoSpecificFilter() throws Exception {
-        cinemaService.fetchAllCinema(null);
+        Cinema cinema1 = new Cinema();
+        Cinema cinema2 = new Cinema();
+
+        List<Cinema> cinemaList = new ArrayList<>();
+        cinemaList.add(cinema1);
+        cinemaList.add(cinema2);
+
+        when(cinemaRepo.findAll()).thenReturn(cinemaList);
+
+        List<Cinema> resultList = cinemaService.fetchAllCinema(null);
+
         verify(cinemaRepo, times(1)).findAll();
+        Assert.assertEquals(cinemaList, resultList);
     }
 
     @Test
     public void fetchAllCinema_shouldReturnAllCinemaWithTheType3D() throws Exception {
-        cinemaService.fetchAllCinema("3D");
+        Cinema cinema1 = new Cinema();
+        cinema1.setType("3D");
+        Cinema cinema2 = new Cinema();
+        cinema2.setType("3D");
+
+        List<Cinema> cinemaList = new ArrayList<>();
+        cinemaList.add(cinema1);
+        cinemaList.add(cinema2);
+
+        when(cinemaRepo.findAllByType(eq("3D"))).thenReturn(cinemaList);
+
+        List<Cinema> resultList = cinemaService.fetchAllCinema("3D");
+
         verify(cinemaRepo, times(1)).findAllByType(eq("3D"));
+        Assert.assertEquals(cinemaList, resultList);
     }
 
     @Test
     public void saveNewCinema_shouldSaveNewCinema() throws Exception {
-        cinemaService.saveNewCinema("Test Cinema", "IMAX");
+        Cinema cinema = new Cinema();
+        cinema.setName("Test Cinema");
+        cinema.setType("IMAX");
+
+        when(cinemaRepo.save(eq(cinema))).thenReturn(cinema);
+
+        Cinema result = cinemaService.saveNewCinema("Test Cinema", "IMAX");
+
         verify(cinemaRepo, times(1)).save(eq(new Cinema()));
+        Assert.assertEquals(cinema, result);
     }
 
     @Test
     public void updateCinema_shouldUpdateTheExistingCinema() throws Exception {
         Cinema cinema = new Cinema();
 
-        cinemaService.updateCinema(cinema,"Cinema 3", "IMAX");
+        when(cinemaRepo.save(eq(cinema))).thenReturn(cinema);
+
+        Cinema result = cinemaService.updateCinema(cinema,"Cinema 3", "IMAX");
+
         verify(cinemaRepo, times(1)).save(eq(cinema));
+        Assert.assertEquals(cinema, result);
     }
 
 }
--- src/test/java/com/synacy/moviehouse/movie/MovieServiceTest.java
+++ src/test/java/com/synacy/moviehouse/movie/MovieServiceTest.java
@@ -1,11 +1,17 @@
 package com.synacy.moviehouse.movie;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -27,65 +33,194 @@ public class MovieServiceTest {
 
     @Test
     public void saveNewMovie_shouldSaveNewMovie() throws Exception {
-        movieService.saveNewMovie("Movie 1", "Horror", 2, "Sample Description");
+        Movie movie = new Movie();
+        movie.setName("Movie 1");
+        movie.setGenre("Horror");
+        movie.setDuration(2);
+        movie.setDescription("Sample Description");
+
+        when(movieRepo.save(eq(movie))).thenReturn(movie);
+
+        Movie result = movieService.saveNewMovie("Movie 1", "Horror", 2, "Sample Description");
+
         verify(movieRepo, times(1)).save(eq(new Movie()));
+        Assert.assertEquals(movie, result);
     }
 
     @Test
     public void fetchAllMovie_shouldReturnAllMovieWithNoSpecificFilter() throws Exception {
-        movieService.fetchAllMovie(null, null);
+        Movie movie1 = new Movie();
+        Movie movie2 = new Movie();
+
+        List<Movie> movieList = new ArrayList<>();
+        movieList.add(movie1);
+        movieList.add(movie2);
+
+        when(movieRepo.findAll()).thenReturn(movieList);
+
+        List<Movie> resultList = movieService.fetchAllMovie(null, null);
+
         verify(movieRepo, times(1)).findAll();
+        Assert.assertEquals(movieList, resultList);
     }
 
     @Test
     public void fetchAllMovie_shouldReturnAllMovieWithFilteredByName() throws Exception {
-        movieService.fetchAllMovie("Movie", null);
-        verify(movieRepo, times(1)).findAllByName("Movie");
+        Movie movie1 = new Movie();
+        movie1.setName("Avengers");
+        Movie movie2 = new Movie();
+        movie2.setName("Avengers");
+
+        List<Movie> movieList = new ArrayList<>();
+        movieList.add(movie1);
+        movieList.add(movie2);
+
+        when(movieRepo.findAllByName("Avengers")).thenReturn(movieList);
+
+        List<Movie> resultList = movieService.fetchAllMovie("Avengers", null);
+
+        verify(movieRepo, times(1)).findAllByName("Avengers");
+        Assert.assertEquals(movieList, resultList);
     }
 
     @Test
     public void fetchAllMovie_shouldReturnAllMovieWithFilteredByGenre() throws Exception {
-        movieService.fetchAllMovie(null, "Horror");
+        Movie movie1 = new Movie();
+        movie1.setGenre("Horror");
+        Movie movie2 = new Movie();
+        movie2.setGenre("Horror");
+
+        List<Movie> movieList = new ArrayList<>();
+        movieList.add(movie1);
+        movieList.add(movie2);
+
+        when(movieRepo.findAllByGenre("Horror")).thenReturn(movieList);
+
+        List<Movie> resultList = movieService.fetchAllMovie(null, "Horror");
+
         verify(movieRepo, times(1)).findAllByGenre("Horror");
+        Assert.assertEquals(movieList, resultList);
     }
 
     @Test
     public void fetchAllMovie_shouldReturnAllMovieWithFilteredByNameAndGenre() throws Exception {
-        movieService.fetchAllMovie("Movie", "Horror");
-        verify(movieRepo, times(1)).findAllByNameAndGenre("Movie", "Horror");
+        Movie movie1 = new Movie();
+        movie1.setName("Avengers");
+        movie1.setGenre("Horror");
+        Movie movie2 = new Movie();
+        movie2.setName("Avengers");
+        movie2.setGenre("Horror");
+
+        List<Movie> movieList = new ArrayList<>();
+        movieList.add(movie1);
+        movieList.add(movie2);
+
+        when(movieRepo.findAllByNameAndGenre("Avengers", "Horror")).thenReturn(movieList);
+
+        List<Movie> resultList = movieService.fetchAllMovie("Avengers", "Horror");
+
+        verify(movieRepo, times(1)).findAllByNameAndGenre("Avengers", "Horror");
+        Assert.assertEquals(movieList, resultList);
     }
 
     @Test
     public void fetchPaginatedMovie_shouldReturnAPaginatedListWithNoSpecificFilter() throws Exception {
-        movieService.fetchPaginatedMovie(null, null, 0, 1);
+        Movie movie1 = new Movie();
+        Movie movie2 = new Movie();
+
+        List<Movie> movieList = new ArrayList<>();
+        movieList.add(movie1);
+        movieList.add(movie2);
+
+        Page<Movie> moviePageList = new PageImpl<>(movieList, new PageRequest(0, 2), movieList.size());
+
+
+        when(movieRepo.findAll(eq(new PageRequest(0, 1)))).thenReturn(moviePageList);
+
+        Page<Movie> resultList = movieService.fetchPaginatedMovie(null, null, 0, 1);
+
         verify(movieRepo, times(1)).findAll(eq(new PageRequest(0, 1)));
+        Assert.assertEquals(moviePageList, resultList);
     }
 
     @Test
     public void fetchPaginatedMovie_shouldReturnAPaginatedListFilteredByName() throws Exception {
-        movieService.fetchPaginatedMovie("Movie", null, 0, 1);
-        verify(movieRepo, times(1)).findAllByName("Movie", new PageRequest(0, 1));
+        Movie movie1 = new Movie();
+        movie1.setName("Avengers");
+        Movie movie2 = new Movie();
+        movie2.setName("Avengers");
+
+        List<Movie> movieList = new ArrayList<>();
+        movieList.add(movie1);
+        movieList.add(movie2);
+
+        Page<Movie> moviePageList = new PageImpl<>(movieList, new PageRequest(0, 2), movieList.size());
+
+
+        when(movieRepo.findAllByName(eq("Avengers"), eq(new PageRequest(0, 1)))).thenReturn(moviePageList);
+
+        Page<Movie> resultList = movieService.fetchPaginatedMovie("Avengers", null, 0, 1);
+
+        verify(movieRepo, times(1)).findAllByName("Avengers", new PageRequest(0, 1));
+        Assert.assertEquals(moviePageList, resultList);
     }
 
     @Test
     public void fetchPaginatedMovie_shouldReturnAPaginatedListFilteredByGenre() throws Exception {
-        movieService.fetchPaginatedMovie(null, "Horror", 0, 1);
+        Movie movie1 = new Movie();
+        movie1.setGenre("Horror");
+        Movie movie2 = new Movie();
+        movie2.setGenre("Horror");
+
+        List<Movie> movieList = new ArrayList<>();
+        movieList.add(movie1);
+        movieList.add(movie2);
+
+        Page<Movie> moviePageList = new PageImpl<>(movieList, new PageRequest(0, 2), movieList.size());
+
+
+        when(movieRepo.findAllByGenre(eq("Horror"), eq(new PageRequest(0, 1)))).thenReturn(moviePageList);
+
+        Page<Movie> resultList = movieService.fetchPaginatedMovie(null, "Horror", 0, 1);
+
         verify(movieRepo, times(1)).findAllByGenre("Horror", new PageRequest(0, 1));
+        Assert.assertEquals(moviePageList, resultList);
     }
 
 
     @Test
     public void fetchPaginatedMovie_shouldReturnAPaginatedListFilteredByNameAndGenre() throws Exception {
-        movieService.fetchPaginatedMovie("Movie", "Horror", 0, 1);
-        verify(movieRepo, times(1)).findAllByNameAndGenre("Movie","Horror", new PageRequest(0, 1));
+        Movie movie1 = new Movie();
+        movie1.setName("Avengers");
+        movie1.setGenre("Horror");
+        Movie movie2 = new Movie();
+        movie2.setName("Avengers");
+        movie2.setGenre("Horror");
+
+        List<Movie> movieList = new ArrayList<>();
+        movieList.add(movie1);
+        movieList.add(movie2);
+
+        Page<Movie> moviePageList = new PageImpl<>(movieList, new PageRequest(0, 2), movieList.size());
+
+        when(movieRepo.findAllByNameAndGenre(eq("Avengers"), eq("Horror"), eq(new PageRequest(0, 1)))).thenReturn(moviePageList);
+
+        Page<Movie> resultList = movieService.fetchPaginatedMovie("Avengers", "Horror", 0, 1);
+
+        verify(movieRepo, times(1)).findAllByNameAndGenre("Avengers","Horror", new PageRequest(0, 1));
+        Assert.assertEquals(moviePageList, resultList);
     }
 
     @Test
     public void updateMovie_shouldUpdateTheExistingMovie() throws Exception {
         Movie movie = new Movie();
 
-        movieService.updateMovie(movie,"Movie 1", "Horror", 2, "Sample Description");
+        when(movieRepo.save(eq(movie))).thenReturn(movie);
+
+        Movie result = movieService.updateMovie(movie,"Movie 1", "Horror", 2, "Sample Description");
+
         verify(movieRepo, times(1)).save(eq(movie));
+        Assert.assertEquals(movie, result);
     }
 
 }
