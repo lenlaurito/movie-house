91bf9a28f3b0a2a042fb3bc37b1889e2f53a89ac
Steven Jhon Gil <steven.gil@synacy.com>
2017-05-18 13:40:32 +0800

    Updated the tests for the CinemaService

--- src/test/java/com/synacy/moviehouse/cinema/CinemaServiceUnitTest.java
+++ src/test/java/com/synacy/moviehouse/cinema/CinemaServiceUnitTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
@@ -29,96 +30,127 @@ public class CinemaServiceUnitTest {
         cinemaService.setCinemaRepository(cinemaRepository);
     }
 
-//    @Test
-//    public void fetchById_cinemaFound_shouldReturnCinema() throws Exception{
-//        long id = 1L;
-//        Cinema cinema = new Cinema();
-//        when(cinemaRepository.findOne(id)).thenReturn(cinema);
-//
-//        cinemaService.fetchById(id);
-//
-//        verify(cinemaRepository,times(1)).findOne(id);
-//        assertEquals(cinema, cinemaRepository.findOne(id));
-//    }
-//
-//    @Test(expected = NoContentFoundException.class)
-//    public void fetchById_noCinemaFound_shouldThrowNoContentFoundException() throws Exception{
-//        long id = 1L;
-//        when(cinemaRepository.findOne(id)).thenReturn(null);
-//
-//        cinemaService.fetchById(id);
-//    }
-//
-//    @Test
-//    public void fetchAll_cinemaFound_shouldReturnCinemaList() throws Exception{
-//        List<Cinema> cinemaList = new ArrayList<>();
-//        cinemaList.add(new Cinema());
-//        cinemaList.add(new Cinema());
-//        when(cinemaRepository.findAll()).thenReturn(cinemaList);
-//
-//        cinemaService.fetchAll();
-//
-//        assertEquals(cinemaList, cinemaRepository.findAll());
-//    }
-//
-//    @Test(expected = NoContentFoundException.class)
-//    public void fetchAll_noCinemaFound_shouldThrowNoContentFoundException() throws Exception{
-//        List<Cinema> cinemaList = new ArrayList<>();
-//        when(cinemaRepository.findAll()).thenReturn(cinemaList);
-//
-//        cinemaService.fetchAll();
-//    }
-//
-//    @Test
-//    public void fetchAllByType_cinemaFound_shouldReturnCinemaListByType() throws Exception{
-//        List<Cinema> cinemaList = new ArrayList<>();
-//        cinemaList.add(new Cinema());
-//        cinemaList.add(new Cinema());
-//        when(cinemaRepository.findAllByType(CinemaType.STANDARD)).thenReturn(cinemaList);
-//
-//        cinemaService.fetchAllByType(CinemaType.STANDARD);
-//
-//        assertEquals(cinemaList, cinemaRepository.findAllByType(CinemaType.STANDARD));
-//    }
-//
-//    @Test(expected = NoContentFoundException.class)
-//    public void fetchAllByType_noCinemaFound_shouldThrowNoContentFoundException() throws Exception{
-//        List<Cinema> cinemaList = new ArrayList<>();
-//        when(cinemaRepository.findAllByType(CinemaType.STANDARD)).thenReturn(cinemaList);
-//
-//        cinemaService.fetchAllByType(CinemaType.STANDARD);
-//    }
-//
-//    @Test
-//    public void createCinema_shouldCreateAndSaveCinema() throws Exception{
-//        Cinema cinema = new Cinema();
-//        when(cinemaRepository.save(cinema)).thenReturn(cinema);
-//
-//        cinemaService.createCinema("name",CinemaType.IMAX);
-//
-//        verify(cinemaRepository).save(cinema);
-//        assertEquals(cinema, cinemaRepository.save(cinema));
-//    }
-//
-//    @Test
-//    public void updateCinema_shouldUpdateAndSaveCinema() throws Exception{
-//        Cinema cinema = new Cinema();
-//        when(cinemaRepository.save(cinema)).thenReturn(cinema);
-//
-//        cinemaService.updateCinema(cinema,"name",CinemaType.IMAX);
-//
-//        verify(cinemaRepository,times(1)).save(cinema);
-//        assertEquals(cinema, cinemaRepository.save(cinema));
-//    }
-//
-//    @Test
-//    public void deleteCinema_shouldDeleteCinema() throws Exception{
-//        Cinema cinema = new Cinema();
-//
-//        cinemaService.deleteCinema(cinema);
-//
-//        verify(cinemaRepository,times(1)).delete(cinema);
-//    }
+    @Test
+    public void fetchById_cinemaFound_shouldReturnCinema() throws Exception{
+        long id = 1L;
+        Cinema expectedCinema = new Cinema();
+        expectedCinema.setName("Name");
+        expectedCinema.setType(CinemaType.STANDARD);
+
+        when(cinemaRepository.findOne(id)).thenReturn(expectedCinema);
+
+        Cinema actualCinema = cinemaService.fetchById(id);
+
+        verify(cinemaRepository,times(1)).findOne(id);
+        assertEquals(expectedCinema.getName(), actualCinema.getName());
+        assertEquals(expectedCinema.getType(), actualCinema.getType());
+    }
+
+    @Test(expected = NoContentFoundException.class)
+    public void fetchById_noCinemaFound_shouldThrowNoContentFoundException() throws Exception{
+        long id = 1L;
+
+        when(cinemaRepository.findOne(id)).thenReturn(null);
+
+        cinemaService.fetchById(id);
+    }
+
+    @Test
+    public void fetchAll_cinemaFound_shouldReturnCinemaList() throws Exception{
+        List<Cinema> expectedCinemaList = new ArrayList<>();
+        expectedCinemaList.add(new Cinema());
+        expectedCinemaList.add(new Cinema());
+
+        when(cinemaRepository.findAll()).thenReturn(expectedCinemaList);
+
+        List<Cinema> actualCinemaList = cinemaService.fetchAll();
+
+        verify(cinemaRepository,times(1)).findAll();
+        assertEquals(expectedCinemaList.size(), actualCinemaList.size());
+    }
+
+    @Test(expected = NoContentFoundException.class)
+    public void fetchAll_noCinemaFound_shouldThrowNoContentFoundException() throws Exception{
+        List<Cinema> cinemaList = new ArrayList<>();
+
+        when(cinemaRepository.findAll()).thenReturn(cinemaList);
+
+        cinemaService.fetchAll();
+    }
+
+    @Test
+    public void fetchAllByType_cinemaFound_shouldReturnCinemaListByType() throws Exception{
+        List<Cinema> expectedCinemaList = new ArrayList<>();
+        expectedCinemaList.add(new Cinema());
+        expectedCinemaList.add(new Cinema());
+
+        when(cinemaRepository.findAllByType(CinemaType.STANDARD)).thenReturn(expectedCinemaList);
+
+        List<Cinema> actualCinemaList = cinemaService.fetchAllByType(CinemaType.STANDARD);
+
+        verify(cinemaRepository,times(1)).findAllByType(CinemaType.STANDARD);
+        assertEquals(expectedCinemaList.size(), actualCinemaList.size());
+    }
+
+    @Test(expected = NoContentFoundException.class)
+    public void fetchAllByType_noCinemaFound_shouldThrowNoContentFoundException() throws Exception{
+        List<Cinema> cinemaList = new ArrayList<>();
+
+        when(cinemaRepository.findAllByType(CinemaType.STANDARD)).thenReturn(cinemaList);
+
+        cinemaService.fetchAllByType(CinemaType.STANDARD);
+    }
+
+    @Test
+    public void createCinema_shouldCreateAndSaveCinema() throws Exception{
+        Cinema expectedCinema = new Cinema();
+        expectedCinema.setName("Name");
+        expectedCinema.setType(CinemaType.STANDARD);
+
+        when(cinemaRepository.save(expectedCinema)).thenReturn(expectedCinema);
+
+        Cinema actualCinema = cinemaService.createCinema("name",CinemaType.IMAX);
+
+        verify(cinemaRepository, times(1)).save(expectedCinema);
+        assertEquals(expectedCinema.getName(), actualCinema.getName());
+        assertEquals(expectedCinema.getType(), actualCinema.getType());
+    }
+
+    @Test
+    public void updateCinema_shouldUpdateAndSaveCinema() throws Exception{
+        Long cinemaId = 1L;
+        String name = "name";
+        CinemaType type = CinemaType.IMAX;
+
+        Cinema expectedCinema = new Cinema();
+        expectedCinema.setName(name);
+        expectedCinema.setType(type);
+
+        when(cinemaRepository.findOne(cinemaId)).thenReturn(expectedCinema);
+        when(cinemaRepository.save(expectedCinema)).thenReturn(expectedCinema);
+
+        Cinema actualCinema = cinemaService.updateCinema(cinemaId, name, type);
+
+        verify(cinemaRepository,times(1)).save(expectedCinema);
+        assertEquals(expectedCinema.getName(), actualCinema.getName());
+        assertEquals(expectedCinema.getType(), actualCinema.getType());
+    }
+
+    @Test
+    public void deleteCinema_shouldDeleteCinema() throws Exception{
+        Long cinemaId = 1L;
+        String name = "name";
+        CinemaType type = CinemaType.IMAX;
+
+        Cinema expectedCinema = new Cinema();
+        expectedCinema.setName(name);
+        expectedCinema.setType(type);
+
+        when(cinemaRepository.findOne(cinemaId)).thenReturn(expectedCinema);
+        cinemaService.deleteCinema(cinemaId);
+
+        verify(cinemaRepository,times(1)).delete(expectedCinema);
+    }
 
 
 }
