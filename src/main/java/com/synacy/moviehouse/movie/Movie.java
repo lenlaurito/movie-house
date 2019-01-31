15bd74f0e03c64c9deee7aba6e160d5a132cdda0
fctmolina <francis.molina@synacy.com>
2019-01-31 10:47:59 +0800

    Add MovieRepositroy

--- src/main/java/com/synacy/moviehouse/movie/Movie.java
+++ src/main/java/com/synacy/moviehouse/movie/Movie.java
@@ -1,14 +1,20 @@
 package com.synacy.moviehouse.movie;
 
 
+import lombok.Data;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Data
 public class Movie {
 
 	@Id @NotNull
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@NotNull
