ddbe78487d5e3827ce857fabb0bbfb7a4122b052
fctmolina <francis.molina@synacy.com>
2019-02-01 18:13:38 +0800

    Add findByDay query in ScheduleRepository.java

--- src/main/java/com/synacy/moviehouse/schedule/ScheduleRepository.java
+++ src/main/java/com/synacy/moviehouse/schedule/ScheduleRepository.java
@@ -2,6 +2,8 @@ package com.synacy.moviehouse.schedule;
 
 import com.synacy.moviehouse.movie.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -11,7 +13,9 @@ import java.util.List;
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     List <Schedule> findByMovie(Movie movie);
-    List<Schedule> findAllByStartDateTimeBetween(Date startDateTime, Date endDateTime);
+
+    @Query("SELECT * FROM Schedule s WHERE NOT (s.startDateTime > ?1 OR s.endDateTime < ?2)")
+    List<Schedule> findByDay(Date start, Date end);
 
     //@Query("SELECT sched FROM Schedule where sched.startDateTime < :date AND sched.endDateTime > :date")
     //List<Schedule> findScheduleByDay(@Param("date") Date date);
