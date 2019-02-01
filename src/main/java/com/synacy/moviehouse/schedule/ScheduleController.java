1ad22883f5cc56257adf481fe8b1c77a03bf41eb
fctmolina <francis.molina@synacy.com>
2019-02-01 16:17:51 +0800

    Create test for createNewMovieSchedule method in ScheduleController.java

--- src/main/java/com/synacy/moviehouse/schedule/ScheduleController.java
+++ src/main/java/com/synacy/moviehouse/schedule/ScheduleController.java
@@ -1,6 +1,9 @@
 package com.synacy.moviehouse.schedule;
 
+import com.synacy.moviehouse.exception.ScheduleAlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +14,10 @@ public class ScheduleController {
     @Autowired
     public ScheduleController(ScheduleService scheduleService) {
         this.scheduleService = scheduleService;
+    }
+
+    @PostMapping("/schedule")
+    public Schedule createNewSchedule(@RequestBody Schedule sched) throws ScheduleAlreadyExistsException {
+        return null;
     }
 }
--- src/test/groovy/com/synacy/moviehouse/schedule/ScheduleControllerSpec.groovy
+++ src/test/groovy/com/synacy/moviehouse/schedule/ScheduleControllerSpec.groovy
@@ -14,4 +14,16 @@ class ScheduleControllerSpec extends Specification {
 
     void cleanup() {}
 
+    def "createNewMovieSchedule should return the newly created Schedule"() {
+        given:
+        Schedule expectedSched = Mock(Schedule)
+
+        scheduleService.createNewSchedule(_ as Schedule) >> expectedSched
+
+        when:
+        Schedule actualSched = scheduleController.createNewSchedule(expectedSched)
+
+        then:
+        expectedSched == actualSched
+    }
 }
