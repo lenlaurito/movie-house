b6bccc4cb70f606dd358f69f32e04dc578276a8f
fctmolina <francis.molina@synacy.com>
2019-02-01 16:49:51 +0800

    Update findByMovie to query schedule based on given movieId

--- src/main/java/com/synacy/moviehouse/schedule/ScheduleRepository.java
+++ src/main/java/com/synacy/moviehouse/schedule/ScheduleRepository.java
@@ -11,7 +11,9 @@ import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    List<Schedule> findByMovie(Movie movie);
+
+    @Query("SELECT sched FROM Schedule where sched.movie == :id")
+    List<Schedule> findByMovie(@Param("id") long id);
 
     @Query("SELECT sched FROM Schedule where sched.startDateTime < :date AND sched.endDateTime > :date")
     List<Schedule> findScheduleByDay(@Param("date") Date date);
